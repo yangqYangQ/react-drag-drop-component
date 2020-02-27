ba83cff6454961d1feab2eb9795ed2aebb554c70
yangq <yangq0979@gmail.com>
2020-02-27 22:12:28 +0800

    完成拖拽组件

--- src/App.js
+++ src/App.js
@@ -1,11 +1,36 @@
 import React, {useReducer} from 'react';
+import './App.css';
 import DragAndDrop from './DragAndDrop';
 
 function App() {
+    const reducer = (state, action) => {
+        switch (action.type) {
+            case 'SET_DROP_DEPTH':
+                return {...state, dropDepth: action.dropDepth};
+            case 'SET_IN_DROP_ZONE':
+                return {...state, inDropZone: action.inDropZone};
+            case 'SET_FILES_TO_DROP':
+                return {...state, fileList: state.fileList.concat(action.filesToDrop)};
+            default:
+                return {...state};
+        }
+    };
+
+    const [data, dispatch] = useReducer(reducer, {
+        dropDepth: 0,        //在拖拽区的文件个数
+        inDropZone: false,   //是否在拖拽区
+        fileList: []         //被拖拽进来的文件
+    });
+
     return (
         <div className="App">
-            <span>React drap and drop component</span>
-            <DragAndDrop/>
+            <h1>React drap and drop component</h1>
+            <DragAndDrop data={data} dispatch={dispatch}/>
+            <ol className='dropedFiles'>
+                {
+                    data.fileList.map((file) => <li key={file.name}>{file.name}</li>)
+                }
+            </ol>
         </div>
     );
 }
--- src/DragAndDrop.js
+++ src/DragAndDrop.js
@@ -2,9 +2,59 @@
 
 import React from 'react';
 
-const dragAndDrop = () => {
+const dragAndDrop = ({data, dispatch}) => {
+    //拖入
+    const handleDragEnter = (e) => {
+        e.preventDefault();
+        e.stopPropagation();
+        dispatch({type: 'SET_DROP_DEPTH', dropDepth: data.dropDepth + 1});
+    };
+
+    //拖出
+    const handleDragLeave = (e) => {
+        e.preventDefault();
+        e.stopPropagation();
+
+        dispatch({type: 'SET_DROP_DEPTH', dropDepth: data.dropDepth - 1});
+        if (data.dropDepth > 0) return;
+        dispatch({type: 'SET_IN_DROP_ZONE', inDropZone: false});
+    };
+
+    //正在当前元素范围内拖拽
+    const handleDragOver = (e) => {
+        e.preventDefault();
+        e.stopPropagation();
+
+        e.dataTransfer.dropEffect = 'copy';
+        dispatch({type: 'SET_IN_DROP_ZONE', inDropZone: true});
+    };
+
+    //完成拖入动作
+    const handleDrop = (e) => {
+        e.preventDefault();
+        e.stopPropagation();
+
+        let files = [...e.dataTransfer.files];
+        if (files && files.length > 0) {
+            const existingFiles = data.fileList.map((file) => file.name);
+            files = files.filter((file) => !existingFiles.includes(file.name));
+
+            dispatch({type: 'SET_FILES_TO_DROP', filesToDrop: files});
+            e.dataTransfer.clearData();
+            dispatch({type: 'SET_DROP_DEPTH', dropDepth: 0});
+            dispatch({type: 'SET_IN_DROP_ZONE', inDropZone: false});
+        }
+    };
+
     return (
-        <>hello </>
+        <div className={data.inDropZone ? 'drag-drop-zone inside-drag-area' : 'drag-drop-zone'}
+             onDragEnter={(e) => handleDragEnter(e)}
+             onDragLeave={(e) => handleDragLeave(e)}
+             onDragOver={(e) => handleDragOver(e)}
+             onDrop={(e) => handleDrop(e)}
+        >
+            <p>Drap files here to Upload</p>
+        </div>
     );
 };
 
