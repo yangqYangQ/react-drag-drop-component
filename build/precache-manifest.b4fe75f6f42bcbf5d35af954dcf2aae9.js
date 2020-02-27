d9aeb6b390196db60c41ab0fa907fcb18bb09466
yangq <yangq0979@gmail.com>
2020-02-27 22:27:48 +0800

    添加 homepage

---
+++ build/precache-manifest.b4fe75f6f42bcbf5d35af954dcf2aae9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ea6726b1a2f3536d0dc1aceb22341f33",
+    "url": "/react-drag-drop-component/build/index.html"
+  },
+  {
+    "revision": "de5e7d7384e44e5023e0",
+    "url": "/react-drag-drop-component/build/static/css/main.5048ef53.chunk.css"
+  },
+  {
+    "revision": "b895d01998c305510285",
+    "url": "/react-drag-drop-component/build/static/js/2.ba9d4dcd.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/react-drag-drop-component/build/static/js/2.ba9d4dcd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "de5e7d7384e44e5023e0",
+    "url": "/react-drag-drop-component/build/static/js/main.9b283eec.chunk.js"
+  },
+  {
+    "revision": "217fcaee8ab1eb3647a0",
+    "url": "/react-drag-drop-component/build/static/js/runtime-main.46d29228.js"
+  }
+]);
